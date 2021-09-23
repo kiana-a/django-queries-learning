daeabd44e8546e220065d7e799ce3a7fc35b39f8
Kiana-a <kiana.ahmadi@gmail.com>
2021-09-23 11:22:13 +0330

    initial commit

---
+++ mobilestore/queries.py
@@ -0,0 +1,31 @@
+from .models import Brand, Mobile
+
+
+def list_all_brands():
+    query = Brand.objects.all()
+    return query
+
+
+def list_all_mobiles():
+    query = Mobile.objects.all()
+    return query
+
+
+def price_of_mobile_with_model(model):
+    query = Mobile.objects.values_list('price','model')
+    return query
+
+
+def most_expensive_mobile():
+    query = Mobile.objects.order_by('-price').first()
+    return query
+
+
+def all_mobiles_with_brand_of(brand_name):
+    query = Mobile.objects.all().values('brand')
+    return query
+
+
+def all_available_mobiles_with_price_in_range(min_price, max_price):
+    query = Mobile.objects.all().filter(is_available=True, price__gte=min_price, price__lte=max_price).count()
+    return query
